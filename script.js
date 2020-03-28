12f9ec2cfb774c450595f743a7d3e3d40b5ffe9b
SamSpiMac <47759163+SamSpiMac@users.noreply.github.com>
2020-03-28 15:56:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,206 @@
+var r = document.getElementById("blk1")
+var a = document.getElementById("blk2")
+var b = document.getElementById("blk3")
+var c = document.getElementById("blk4")
+var d = document.getElementById("blk5")
+var e = document.getElementById("blk6")
+var f = document.getElementById("blk7")
+var g = document.getElementById("blk8")
+var h = document.getElementById("blk9")
+var div = documente.creaetElement("div")
+div.style.position = absolute
+div.style.backgroundColor = darkgray
+
+var i = true
+
+function onOff1() {
+    i = teste()  
+}
+
+function teste() {
+    if(i != true) {
+        azul()
+        return true      
+    } else {
+        verm() 
+        return false 
+    }
+}
+function azul() {
+    r.style.backgroundColor = "#005580"
+}
+
+function verm() {
+    r.style.backgroundColor = "#b81414"
+}
+
+function onOff2() {
+    i = teste2()  
+}
+
+function teste2() {
+    if(i != true) {
+        azul2()
+        return true      
+    } else {
+        verm2() 
+        return false 
+    }
+}
+function azul2() {
+    a.style.backgroundColor = "#005580"
+}
+
+function verm2() {
+    a.style.backgroundColor = "#b81414"
+}
+
+function onOff3() {
+    i = teste3()  
+}
+
+function teste3() {
+    if(i != true) {
+        azul3()
+        return true      
+    } else {
+        verm3() 
+        return false 
+    }
+}
+function azul3() {
+    b.style.backgroundColor = "#005580"
+}
+
+function verm3() {
+    b.style.backgroundColor = "#b81414"
+}
+
+function onOff4() {
+    i = teste4()  
+}
+
+function teste4() {
+    if(i != true) {
+        azul4()
+        return true      
+    } else {
+        verm4() 
+        return false 
+    }
+}
+function azul4() {
+    c.style.backgroundColor = "#005580"
+}
+
+function verm4() {
+    c.style.backgroundColor = "#b81414"
+}
+
+function onOff5() {
+    i = teste5()  
+}
+
+function teste5() {
+    if(i != true) {
+        azul5()
+        return true      
+    } else {
+        verm5() 
+        return false 
+    }
+}
+function azul5() {
+    d.style.backgroundColor = "#005580"
+}
+
+function verm5() {
+    d.style.backgroundColor = "#b81414"
+}
+
+function onOff6() {
+    i = teste6()  
+}
+
+function teste6() {
+    if(i != true) {
+        azul6()
+        return true      
+    } else {
+        verm6() 
+        return false 
+    }
+}
+function azul6() {
+    e.style.backgroundColor = "#005580"
+}
+
+function verm6() {
+    e.style.backgroundColor = "#b81414"
+}
+
+function onOff7() {
+    i = teste7()  
+}
+
+function teste7() {
+    if(i != true) {
+        azul7()
+        return true      
+    } else {
+        verm7() 
+        return false 
+    }
+}
+function azul7() {
+    f.style.backgroundColor = "#005580"
+}
+
+function verm7() {
+    f.style.backgroundColor = "#b81414"
+}
+
+function onOff8() {
+    i = teste8()  
+}
+
+function teste8() {
+    if(i != true) {
+        azul8()
+        return true      
+    } else {
+        verm8() 
+        return false 
+    }
+}
+function azul8() {
+    g.style.backgroundColor = "#005580"
+}
+
+function verm8() {
+    g.style.backgroundColor = "#b81414"
+}
+
+function onOff9() {
+    i = teste9()  
+}
+
+function teste9() {
+    if(i != true) {
+        azul9()
+        return true      
+    } else {
+        verm9() 
+        return false 
+    }
+}
+function azul9() {
+    h.style.backgroundColor = "#005580"
+}
+
+function verm9() {
+    h.style.backgroundColor = "#b81414"
+}
+
+
+console.log(i)
